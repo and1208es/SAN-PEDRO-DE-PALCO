54b071d7f7d5b8c57d998e45a98c9937d86b228d
and1208es <and1208es@gmail.com>
2021-07-19 07:48:06 -0500

    municipalidad

--- exec.sh
+++ exec.sh
@@ -2,3 +2,5 @@ ejs pages/index.ejs -o public/index.html
 ejs pages/servicios.ejs -o public/servicios.html
 ejs pages/eventos.ejs -o public/eventos.html
 ejs pages/obras.ejs -o public/obras.html
+ejs pages/municipalidad.ejs -o public/municipalidad.html
+ejs pages/organigrama.ejs -o public/organigrama.html
